f95efa2c5bfce87a4771105163e32599f0ca232c
itapuafighter <81306498+itapuafighter@users.noreply.github.com>
2026-03-14 13:57:42 +0100

    Set charset to 'utf8mb4' in database connection
    Add charset configuration for database connection

--- model/alimentosModel.js
+++ model/alimentosModel.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'Comida'
+  database: process.env.DB_NAME || 'Comida',
+  charset: 'utf8mb4'
 });
 
 connection.connect((err) => {
